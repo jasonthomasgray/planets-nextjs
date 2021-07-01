7aa128526097eec8cd8984a797e6c0eb76a13e9b
Jason Gray <jasonthomasgray@gmail.com>
2021-07-01 15:28:35 +0100

    skeleton data for the 13 /(dwarf )?planets/

--- pages/api/planets.ts
+++ pages/api/planets.ts
@@ -23,6 +23,78 @@ export default function handler(
         age: 4.503,
         solarMass: 0.055,
       },
+      {
+        name: 'Venus',
+        picture: '/images/venus.jpg',
+        age: 4.503,
+        solarMass: 0.815,
+      },
+      {
+        name: 'Earth',
+        picture: '/images/earth.jpg',
+        age: 0,
+        solarMass: 0,
+      },
+      {
+        name: 'Mars',
+        picture: '/images/mars.jpg',
+        age: 0,
+        solarMass: 0,
+      },
+      {
+        name: 'Jupiter',
+        picture: '/images/jupiter.jpg',
+        age: 0,
+        solarMass: 0,
+      },
+      {
+        name: 'Saturn',
+        picture: '/images/saturn.jpg',
+        age: 0,
+        solarMass: 0,
+      },
+      {
+        name: 'Uranus',
+        picture: '/images/uranus.jpg',
+        age: 0,
+        solarMass: 0,
+      },
+      {
+        name: 'Neptune',
+        picture: '/images/neptune.jpg',
+        age: 0,
+        solarMass: 0,
+      },
+      {
+        name: 'Pluto',
+        picture: '/images/pluto.jpg',
+        age: 0,
+        solarMass: 0,
+      },
+      {
+        name: 'Ceres',
+        picture: '/images/ceres.jpg',
+        age: 0,
+        solarMass: 0,
+      },
+      {
+        name: 'Makemake',
+        picture: '/images/makemake.jpg',
+        age: 0,
+        solarMass: 0,
+      },
+      {
+        name: 'Haumea',
+        picture: '/images/haumea.jpg',
+        age: 0,
+        solarMass: 0,
+      },
+      {
+        name: 'Eris',
+        picture: '/images/eris.jpg',
+        age: 0,
+        solarMass: 0,
+      },
     ],
   });
 }
